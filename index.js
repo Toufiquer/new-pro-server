51df9e6b1c3df6a14727e1cd64f0f336dfcada6c
Toufiquer Tahman <toufiquerabc@gmail.com>
2022-05-24 17:47:48 +0600

    75 5

--- index.js
+++ index.js
@@ -9,6 +9,21 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 app.use(express.json());
 app.use(cors());
 
+const verifyJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: "UnAuthorized Access" });
+    }
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: "Forbidden Access" });
+        }
+        req.decoded = decoded;
+        next();
+    });
+};
+
 const uri = `mongodb+srv://${process.env.APP_USER}:${process.env.APP_PASSWORD}@cluster0.qlqtd.mongodb.net/?retryWrites=true&w=majority`;
 async function runServer() {
     const client = new MongoClient(uri, {
@@ -69,14 +84,23 @@ async function runServer() {
             res.send(services);
             // -- -- -- -- -
         });
-        app.get("/dashboard", async (req, res) => {
+        app.get("/dashboard", verifyJWT, async (req, res) => {
             const patient = req.query.treatment;
-            console.log(patient);
-            const query = { patient: patient };
-            // const query = {};
-            const booking = bookingCollection.find(query);
-            const cursor = await booking.toArray();
-            res.send(cursor);
+
+            console.log(req.decoded);
+            const decodedEmail = req.decoded.email;
+            if (patient === decodedEmail) {
+                // const tokenBearer = req.headers.authorization;
+                // const token = tokenBearer.split(" ")[1];
+                // console.log("token", token);
+                const query = { patient: patient };
+                // const query = {};
+                const booking = bookingCollection.find(query);
+                const cursor = await booking.toArray();
+                return res.send(cursor);
+            } else {
+                return res.status(403).send({ message: "Forbidden Access" });
+            }
         });
         app.get("/service", async (req, res) => {
             const query = {};
@@ -91,20 +115,25 @@ async function runServer() {
         //     res.send(collection);
         // });
 
-        app.post("/booking", async (req, res) => {
+        app.post("/booking", verifyJWT, async (req, res) => {
             const booking = req.body;
-            console.log(booking);
-            const query = {
-                treatment: booking.treatment,
-                date: booking.date,
-                patient: booking.patient,
-            };
-            const exists = await bookingCollection.findOne(query);
-            if (exists) {
-                return res.send({ success: false, booking: exists });
+            const decodedEmail = req.decoded.email;
+            console.log("decoded", booking.patient);
+            if (booking.patient === decodedEmail) {
+                const query = {
+                    treatment: booking.treatment,
+                    date: booking.date,
+                    patient: booking.patient,
+                };
+                const exists = await bookingCollection.findOne(query);
+                if (exists) {
+                    return res.send({ success: false, booking: exists });
+                }
+                const result = await bookingCollection.insertOne(booking);
+                return res.send({ success: true, result });
+            } else {
+                return res.status(403).send({ message: "Forbidden Access" });
             }
-            const result = await bookingCollection.insertOne(booking);
-            res.send({ success: true, result });
         });
         app.get("/", (req, res) => {
             res.send("Node is working.");
