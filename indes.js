0bfc869ad81cfdb092b84e7de69c1bf276d71745
Toufiquer Tahman <toufiquerabc@gmail.com>
2022-05-25 17:04:19 +0600

    75 heroku

--- indes.js
+++ indes.js
@@ -1,142 +1,221 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
-const port = process.env.PORT || 3500;
-const app = express();
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const jwt = require("jsonwebtoken");
-// Connect to react-app | middleware
-app.use(cors());
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 3500;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+// Middle were;
 app.use(express.json());
+app.use(cors());
 
-// Verify JWT
-function verifyJWT(req, res, next) {
+const verifyJWT = (req, res, next) => {
     const authHeader = req.headers.authorization;
     if (!authHeader) {
-        return res.status(401).send({ massage: "unauthorized" });
+        return res.status(401).send({ message: "UnAuthorized Access" });
     }
     const token = authHeader.split(" ")[1];
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
         if (err) {
-            return res.status(403).send({ massage: "Forbidden access" });
+            return res.status(403).send({ message: "Forbidden Access" });
         }
-        // console.log("decoded", decoded);
         req.decoded = decoded;
-        console.log("Inside JWTVerify", authHeader);
         next();
     });
-}
+};
 
-const uri = `mongodb+srv://${process.env.DB_ID}:${process.env.DB_PASSWORD}@cluster0.je7gu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    serverApi: ServerApiVersion.v1,
-});
-
-async function runDB() {
+const uri = `mongodb+srv://${process.env.APP_USER}:${process.env.APP_PASSWORD}@cluster0.qlqtd.mongodb.net/?retryWrites=true&w=majority`;
+async function runServer() {
+    const client = new MongoClient(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        serverApi: ServerApiVersion.v1,
+    });
     try {
         await client.connect();
-        console.log("mongodb connected");
-        const database = client.db("Assignment11").collection("AllFruits");
-        // Auth
-        app.post("/login", async (req, res) => {
-            const user = req.body;
-            const accessToken = jwt.sign(
-                user,
-                process.env.ACCESS_TOKEN_SECRET,
-                { expiresIn: "99d" }
-            );
-            res.send({ accessToken });
-            console.log(user);
-        });
 
-        // Add New Product
-        app.post("/addProduct", async (req, res) => {
-            const productData = req.body;
-            database.insertOne(productData.userData);
-            res.send("Your Product is Successfully saved.");
-        });
+        const servicesCollection = client
+            .db("servicesCollection")
+            .collection("services");
+        const bookingCollection = client
+            .db("bookingCollection")
+            .collection("booking");
+        const userCollection = client.db("userCollection").collection("user");
 
-        // Get All Product
-        app.get("/products", async (req, res) => {
-            let query = {};
-            // console.log(email, query);
-            const cursor = database.find(query);
-            const result = await cursor.toArray();
-            // console.log(result);
-            res.send(result);
-        });
-        // Get All Product
-        // Verify jwt is off
-        app.get("/myItem", async (req, res) => {
-            // const decodedEmail = req.decoded.email;
-            // console.log(decodedEmail);
-            const email = req.query.email;
-            let query = {};
-            if (email) {
-                query = { email: email };
+        app.get("/user", async (req, res) => {
+            const result = await userCollection.findOne({
+                email: req.query.email,
+            });
+            const role = result?.role;
+            // console.log("result", result);
+            if (role === "admin") {
+                return res.send({ result: true });
+            } else {
+                return res
+                    .status(403)
+                    .send({ result: false, message: "Forbidden Access" });
             }
-            // console.log(email, query);
-            const cursor = database.find(query);
-            const result = await cursor.toArray();
-            // console.log(result);
-            res.send(result);
         });
 
-        // Delete User
-        app.delete("/products/:id", async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const result = await database.deleteOne(query);
-            res.send(result);
+        // add User use in useToken
+        app.put("/user", async (req, res) => {
+            const name = req.body.name;
+            const email = req.body.email;
+            const user = { name, email };
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = { $set: user };
+            const result = await userCollection.updateOne(
+                filter,
+                updateDoc,
+                options
+            );
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN, {
+                expiresIn: "1h",
+            });
+            res.send({ result, token });
         });
 
-        // Find One Data
-        app.get("/products/:id", async (req, res) => {
-            const id = req.params.id;
-            // console.log(id);
-            const query = { _id: ObjectId(id) };
-            const result = await database.findOne(query);
+        app.get("/allUsers", verifyJWT, async (req, res) => {
+            const admin = await userCollection.findOne({
+                email: req.query.email,
+            });
+            const role = admin?.role;
+            console.log("result", admin);
+            if (role === "admin") {
+                const result = await userCollection.find().toArray();
+                res.send(result);
+            } else {
+                return res
+                    .status(403)
+                    .send({ result: false, message: "Forbidden Access" });
+            }
+        });
+        app.delete("/delete", async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const result = await userCollection.deleteOne(query);
             res.send(result);
         });
-
-        // Update || PUT
-        app.put("/products/:id", async (req, res) => {
-            const id = req.params.id;
-            const updateData = req.body;
-            // console.log(updateData);
-            const query = { _id: ObjectId(id) };
+        app.put("/mkAdmin", async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const findOne = await userCollection.findOne(query);
             const options = { upsert: true };
             const updateDoc = {
                 $set: {
-                    name: updateData.name,
-                    email: updateData.email,
-                    productName: updateData.productName,
-                    productImg: updateData.productImg,
-                    productDescription: updateData.productDescription,
-                    productQuantity: updateData.productQuantity,
-                    productPrice: updateData.productPrice,
+                    name: findOne.name,
+                    email: findOne.email,
+                    role: "admin",
                 },
             };
-            // console.log(updateDoc);
-            const result = await database.updateOne(query, updateDoc, options);
-            // console.log(
-            //     `${result.matchedCount} document(s) matched the filter, updated ${result.modifiedCount} document(s)`
-            // );
+            const result = await userCollection.updateOne(
+                query,
+                updateDoc,
+                options
+            );
             res.send(result);
         });
+        app.put("/mkClient", async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const findOne = await userCollection.findOne(query);
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    name: findOne.name,
+                    email: findOne.email,
+                    role: "client",
+                },
+            };
+            const result = await userCollection.updateOne(
+                query,
+                updateDoc,
+                options
+            );
+            res.send(result);
+        });
+        app.get("/available", async (req, res) => {
+            // working
+            const date = req.query.date || "May 23, 2022";
+            // console.log(req.query);
+            const services = await servicesCollection.find().toArray();
+            const query = { date: date };
+            const booking = await bookingCollection.find(query).toArray();
+
+            services.forEach(service => {
+                const serviceBooking = booking.filter(
+                    b => b.treatment === service.name
+                );
+                const booked = serviceBooking.map(s => s.slot);
+                service.booked = booked;
+                const available = service.slots.filter(
+                    slot => !booked.includes(slot)
+                );
+                service.available = available;
+            });
+
+            res.send(services);
+            // -- -- -- -- -
+        });
+        app.get("/dashboard", verifyJWT, async (req, res) => {
+            const patient = req.query.treatment;
+
+            // console.log(req.decoded);
+            const decodedEmail = req.decoded.email;
+            if (patient === decodedEmail) {
+                // const tokenBearer = req.headers.authorization;
+                // const token = tokenBearer.split(" ")[1];
+                // console.log("token", token);
+                const query = { patient: patient };
+                // const query = {};
+                const booking = bookingCollection.find(query);
+                const cursor = await booking.toArray();
+                return res.send(cursor);
+            } else {
+                return res.status(403).send({ message: "Forbidden Access" });
+            }
+        });
+        app.get("/service", async (req, res) => {
+            const query = {};
+            const cursor = servicesCollection.find(query);
+            const collection = await cursor.toArray();
+            res.send(collection);
+        });
+        // app.get("/serviceSlot", async (req, res) => {
+        //     const query = {};
+        //     const cursor = servicesSlotsCollection.find(query);
+        //     const collection = await cursor.toArray();
+        //     res.send(collection);
+        // });
+
+        app.post("/booking", verifyJWT, async (req, res) => {
+            const booking = req.body;
+            const decodedEmail = req.decoded.email;
+            // console.log("decoded", booking.patient);
+            if (booking.patient === decodedEmail) {
+                const query = {
+                    treatment: booking.treatment,
+                    date: booking.date,
+                    patient: booking.patient,
+                };
+                const exists = await bookingCollection.findOne(query);
+                if (exists) {
+                    return res.send({ success: false, booking: exists });
+                }
+                const result = await bookingCollection.insertOne(booking);
+                return res.send({ success: true, result });
+            } else {
+                return res.status(403).send({ message: "Forbidden Access" });
+            }
+        });
     } finally {
-        // await client.close();
+        app.get("/", (req, res) => {
+            res.send("Node is working.");
+        });
     }
 }
-
-runDB().catch(console.dir);
-
-app.get("/", (req, res) => {
-    res.send("Node server is working");
-});
-
+runServer().catch(console.error);
 app.listen(port, () => {
-    console.log("Listening", port);
+    console.log("listening to the port ", port);
 });
